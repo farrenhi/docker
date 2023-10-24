30e44bdb7bf5b38f3d259f066040df19bf1b4304
farrenhi <abcd0425.com>
2023-10-24 22:06:20 +0800

    env and docker compose are good now

--- app/app.py
+++ app/app.py
@@ -8,16 +8,20 @@ load_dotenv()  # Load variables from .env file
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
 
-greet = os.getenv('greeting')
+greet = os.getenv('greet')
 port_number = os.getenv('port_number')
 
-print(greet)
-print(port_number)
+print("greet:", greet)
+print("port_number:", port_number)
+
 
 # Pages
 @app.route("/")
 def index():
-    return "Hello Docker! Yes! Yes! Yes!" # + port_number
+    print("greet:", greet)
+    print("port_number:", port_number)
+    return port_number
+    return "Hello Docker! Yessssss" + port_number
 	# return render_template("index.html")
 
 app.run(host="0.0.0.0")
