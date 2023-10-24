165d5bf533d1fc2a318f28db429dac94d398a2a5
t480s <abcd0425@gmail.com>
2023-10-24 15:10:40 +0800

    done on docker. next is to try compose

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import *
-app=Flask(__name__)
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+load_dotenv()  # Load variables from .env file
+
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
+
+greet = os.getenv('greeting')
+port_number = os.getenv('port_number')
+
+print(greet)
+print(port_number)
 
 # Pages
 @app.route("/")
 def index():
-    return 'Hello, World!'
+    return "Hello Docker!" + port_number
 	# return render_template("index.html")
 
-
-app.run(host="0.0.0.0", port=3000, debug=True)
+app.run(host="0.0.0.0", port=5000, debug=True)
